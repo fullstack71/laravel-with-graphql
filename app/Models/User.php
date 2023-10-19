df497aab8b4e63413f0dd91681b489373129f3de
fullstack71 <developerfullstack71@gmail.com>
2023-10-19 07:37:43 +0600

    Local Scopes

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,5 +13,9 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function scopeVerified(Builder $query)
+    {
+        return $query->whereNull('email_verified_at');
+    }
 
 }
